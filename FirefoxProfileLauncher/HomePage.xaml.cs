9509a87244cace970fc4204ee31f7f10dcb1b2c5
PawelWielga <wielgapawel@gmail.com>
2024-03-26 18:03:06 +0100

    change winForms to wpf

---
+++ FirefoxProfileLauncher/HomePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FirefoxProfileLauncher;
+
+public partial class HomePage : Page
+{
+    private string _firefoxBaseUrl = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+    private string _runProfileArgument = "-P";
+    private string _userProfileName1 = "Pawełek";
+    private string _userProfileName2 = "Ewelinka";
+
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click1(object sender, RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start(_firefoxBaseUrl, _runProfileArgument + " " + _userProfileName1);
+    }
+
+
+    private void Button_Click2(object sender, RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start(_firefoxBaseUrl, _runProfileArgument + " " + _userProfileName2);
+    }
+}
---
+++ FirefoxProfileLauncher/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace FirefoxProfileLauncher;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        MainFrame.Content = new HomePage();
+    }
+}
